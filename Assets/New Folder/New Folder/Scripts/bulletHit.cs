f847e71d4474335e4a1bc2566ef2aa00968f9b4f
IKE37 <92145145+IKE37@users.noreply.github.com>
2022-09-17 03:07:49 +0700

    first commit

---
+++ Assets/New Folder/New Folder/Scripts/bulletHit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletHit : MonoBehaviour {
+
+	public float weaponDamgage;
+
+	projectileController myPC;
+
+	public GameObject bulletExplosion;
+
+	void Awake(){
+		myPC = GetComponentInParent<projectileController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Shootable") {
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position, transform.rotation);
+			Destroy (gameObject);
+			if (other.gameObject.layer == LayerMask.NameToLayer ("enemy")) {
+				enemyHealth hurtEnemy = other.gameObject.GetComponent<enemyHealth> ();
+				hurtEnemy.addDamge (weaponDamgage);
+			}
+		}
+	}
+
+	void OnTringgerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Shootable") {
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position, transform.rotation);
+			Destroy (gameObject);
+			if (other.gameObject.layer == LayerMask.NameToLayer ("enemy")) {
+				enemyHealth hurtEnemy = other.gameObject.GetComponent<enemyHealth> ();
+				hurtEnemy.addDamge (weaponDamgage);
+			}
+		}
+	}
+}
---
+++ Assets/New Folder/New Folder/Scripts/playerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+	public float maxHealth;
+	float currentHealth;
+
+	public GameObject bloodEffect;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addDamage(float damage){
+		if (damage <= 0)
+			return;
+		currentHealth -= damage;
+
+		if (currentHealth <= 0)
+			makeDead ();
+	}
+
+	void makeDead(){
+		Instantiate (bloodEffect, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
